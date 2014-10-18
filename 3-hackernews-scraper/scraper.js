0582e042eb4f0d7c9c1f455b26f30e2fa6ce8abc
asenovm <asenov.m@gmail.com>
2014-10-18 13:27:35 +0300

    * We are now actually scraping articles

--- 3-hackernews-scraper/scraper.js
+++ 3-hackernews-scraper/scraper.js
@@ -1,5 +1,7 @@
 var db = require('./db'),
-    TIMEOUT_POLLING = 2000;
+    request = require('request'),
+    API_END_POINT_MAX_ITEM = 'https://hacker-news.firebaseio.com/v0/maxitem.json',
+    TIMEOUT_POLLING = 30000;
 
 startPolling();
 
@@ -10,5 +12,25 @@ function startPolling() {
 
 function fetchAndWriteArticles() {
     var maxItem = db.getMaxItem();
-    console.log('max item is ' + maxItem);
+    request.get(API_END_POINT_MAX_ITEM, function (err, data, body) {
+        var newMaxItem = parseInt(body, 10);
+        db.setMaxItem(newMaxItem);
+        if(!maxItem) {
+            fetchAndWriteArticleWithId(newMaxItem);
+        } else {
+            for(var i = maxItem; i <= newMaxItem; ++i) {
+                fetchAndWriteArticleWithId(i);
+            }
+        }
+    });
+}
+
+function fetchAndWriteArticleWithId(id) {
+    request.get(_getApiEndPointForArticle(id), function (err, data, body) {
+        db.writeArticle(JSON.parse(body));
+    });
+}
+
+function _getApiEndPointForArticle(id) {
+    return 'https://hacker-news.firebaseio.com/v0/item/' + id + '.json'
 }
